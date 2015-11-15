edecc9f2105f0629417e8c02809106df697b0a9f
Carlyle Davis <carlyle@Carlyles-MacBook-Pro-2.local>
2015-11-15 16:24:39 -0500

    updated notebook and ReadME

---
+++ ADS_Foundations/README.md
@@ -0,0 +1,19 @@
+# Applied Data Science Foundations Project 
+**Ideological Effects of Gerrymandering**
+Carlyle Davis & Maxwell Feinglass
+
+
+CSVs for the iPython Notebook can be [found here](http://bit.ly/1X0ViNe).
+
+
+In the Winter of 1812, the then Governor of Massachusetts, a certain Elbridge Gerry, signed a bill into law creating the new Massachusetts State Senate district of South Essex.  In doing so, he used the process of redrawing legislative districts to directly benefit his Republican-Democrat Party at the expense of the opposition Federalist Party.  Gerry lost his reelection bid and the Federalists swept into power the following term, but his redistricting efforts left the Massachusetts State Senate in the firm control of his Democratic-Republican allies.  Crying foul, the Federalists decried this tactic, labeling it a “Gerry-Mander” as the redrawn district’s created by Gerry looked like a salamander.  More than two hundred years later, the ‘gerrymander’ has become a hallmark of American politics.
+
+The Original “Gerry-Mander” as Salamander Monster:
+![alt tag](http://s.newsweek.com/sites/www.newsweek.com/files/2015/08/03/0803gerrymandering01.jpg)
+
+Since Governor Gerry’s early attempt to gain partisan advantage, the methods used to ensure the ruling party maintains its hold on power through redistricting has become more and more sophisticated.  With the rise of big data and modern geographical information systems, “Democrats are under-represented by about 18 seats in the House, relative to their vote share” as the result of Gerrymandering in 2012 (Ingman, 2014).  This under representation, something totally unanticipated by the founders, may be an underlying cause of the legislative and cultural polarization of the last decade.  Norm Orintsein, writing for the National Journal explains:
+
+Gerrymandering has leached much of the broader heterogeneity out of congressional districts, contributing to the echo-chamber effect, where members’ ideological predilections are reinforced, and not challenged, back home…  Partisan gerrymandering skews results away from the broader sentiments of voters in a state… has helped create a huge number of districts that are fundamentally safe for one party… and adds to the homogeneity of districts making low-turnout primaries dominated by ideological activists the only meaningful elections.  More broadly, gerrymandering moves House and state legislative elections away from any meaningful responsiveness to the will of the people. And the pattern of lawmakers choosing their voters instead of voters choosing their lawmakers creates more disaffection and cynicism among the public.  (Ornstien, 2014).
+
+Modern gerrymandering is a fundamentally data driven exercise.  Therefore, this paper attempts to use statistical learning to characterize how the gerrymandering process is ideology sorting the United States.
+
--- ADS_Foundations/pca_and_clustering.py
+++ ADS_Foundations/pca_and_clustering.py
@@ -1,6 +1,7 @@
 ###
 #
 # Code for Visualizations for ADS Foundations Project
+# partner: Max Feinglass
 # Ideological Effects of Gerrymandering
 #
 # Carlyle Davis
@@ -131,5 +132,73 @@ ax.set_xlabel('PC-1')
 ax.set_ylabel('PC-2')
 ax.set_zlabel('PC-3')
 ax.set_title("Clustering of Gerrymandering components, n = {0}".format(clusters))
+
+pl.show()
+
+###
+#
+# Sillouette plot
+# source: http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+#
+###
+n_clusters = 14
+
+# Create a subplot with 1 row and 2 columns
+fig = pl.figure(figsize = (12,12))#figsize=pl.figaspect(2.))
+ax1 = fig.add_subplot(1, 1, 1)
+# The 1st subplot is the silhouette plot
+# The silhouette coefficient can range from -1, 1 but in this example all
+# lie within [-0.1, 1]
+ax1.set_xlim([-0, 1])
+# The (n_clusters+1)*10 is for inserting blank space between silhouette
+# plots of individual clusters, to demarcate them clearly.
+ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+# Initialize the clusterer with n_clusters value and a random generator
+# seed of 10 for reproducibility.
+clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+cluster_labels = clusterer.fit_predict(X)
+
+# The silhouette_score gives the average value for all the samples.
+# This gives a perspective into the density and separation of the formed
+# clusters
+silhouette_avg = silhouette_score(X, cluster_labels)
+print("For n_clusters =", n_clusters,
+      "The average silhouette_score is :", silhouette_avg)
+
+# Compute the silhouette scores for each sample
+sample_silhouette_values = silhouette_samples(X, cluster_labels)
+y_lower = 10
+for i in range(n_clusters):
+    # Aggregate the silhouette scores for samples belonging to
+    # cluster i, and sort them
+    ith_cluster_silhouette_values = \
+        sample_silhouette_values[cluster_labels == i]
+
+    ith_cluster_silhouette_values.sort()
+
+size_cluster_i = ith_cluster_silhouette_values.shape[0]
+    y_upper = y_lower + size_cluster_i
+    
+    color = cm.spectral(float(i) / n_clusters)
+    ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                      0, ith_cluster_silhouette_values,
+                      facecolor=color, edgecolor=color, alpha=0.7)
+        
+                      # Label the silhouette plots with their cluster numbers at the middle
+                      ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+                      
+                      # Compute the new y_lower for next plot
+    y_lower = y_upper + 10  # 10 for the 0 samples
+
+ax1.set_title("The Silhouette Plot: 14 Clusters")
+ax1.set_xlabel("The silhouette coefficient values")
+ax1.set_ylabel("Cluster label")
+
+# The vertical line for average silhoutte score of all the values
+ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+ax1.set_yticks([])  # Clear the yaxis labels / ticks
+ax1.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
 
 pl.show()
